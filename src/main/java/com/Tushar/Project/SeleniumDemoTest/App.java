2ddafddba37dcd79c3614c993bbed3d1732d311c
tushartone <tushartone24@gmail.com>
2018-06-11 18:41:37 -0700

    first commit

---
+++ src/main/java/com/Tushar/Project/SeleniumDemoTest/App.java
@@ -0,0 +1,13 @@
+package com.Tushar.Project.SeleniumDemoTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/Tushar/Project/SeleniumDemoTest/DemoTest.java
@@ -0,0 +1,27 @@
+package com.Tushar.Project.SeleniumDemoTest;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DemoTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.setProperty("webdriver.chrome.driver", ".//driver//chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.get("http://www.google.com");
+		
+		String title = driver.getTitle();
+		System.out.println(title);
+		driver.quit();
+	}
+
+}
